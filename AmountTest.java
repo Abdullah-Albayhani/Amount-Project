9e44baa48f491646e90da984fafd7faf23127402
Abdullah-Albayhani <112866278+Abdullah-Albayhani@users.noreply.github.com>
2023-02-20 17:27:38 +0300

    Update AmountTest.java

--- AmountTest.java
+++ AmountTest.java
@@ -26,16 +26,3 @@ public class AmountTest {
     }
     
 }
-
-
-
-
-
-
-/* 
-for(int i = 0; i < 1; i++){
-    System.out.println("----------New Amount----------");
-    Amount.readData();
-    Amount.enterAmount();
-     }
-     */
